57bfd2d8628c372d34a42ed12bf1a84d8553e78e
SenLiang Deng <tommy360928994@gmail.com>
2026-08-22 18:11:52 -0500

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './sections/Hero/Hero'
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
+      <main>
+        <Hero/>
+      </main>
+    
+    
+    
     </div>
+    
+    
   )
 }
 
