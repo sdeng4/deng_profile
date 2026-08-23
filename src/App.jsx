64bb3d5e3931742717294d93fb7743a994d23703
SenLiang Deng <tommy360928994@gmail.com>
2026-08-23 13:09:01 -0500

    add about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './sections/Hero/Hero'
+import About from './sections/About/About'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <main>
         <Hero/>
+        <About/>
       </main>
     
     
